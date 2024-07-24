b01b5a37bcf9e29e0488e1b2873e556d9482960f
ajaax1 <ruanhigor123@gmail.com>
2024-07-23 22:34:00 -0300

    testar

--- src/assets/axiosPost.js
+++ src/assets/axiosPost.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
-import { encode, decode } from 'base-64';
+import { encode } from 'base-64';
 
 const client_id = import.meta.env.VITE_CLIENT_ID;
 const client_secret = import.meta.env.VITE_CLIENT_SECRET;
 const auth = `${client_id}:${client_secret}`;
 const authHeader = 'Basic ' + encode(auth);
 
-const spotifyPost = (rota,body) => {
-  axios.post(`https://accounts.spotify.com/${rota}`, {
-    body,
+const spotifyPost = (rota, body) => {
+  const params = new URLSearchParams(body);
+  axios.post(`https://accounts.spotify.com/${rota}`, params, {
     headers: {
       'Authorization': authHeader,
       'Content-Type': 'application/x-www-form-urlencoded'
@@ -21,7 +21,7 @@ const spotifyPost = (rota,body) => {
     console.error(error);
   })
   .finally(function () {
-
-  });  
+  });
 }
+
 export default spotifyPost;
